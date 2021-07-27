1b574cddae8a9ae2d93fd75ce041504401024d6c
somnath chouwdhury <somnathchouwdhury7@gmail.com>
2021-07-27 08:20:05 +0530

    Update ctt.py

--- ctt.py
+++ ctt.py
@@ -1,19 +1,9 @@
-#!/bin/python3
 
 import math
 import os
 import random
 import re
 import sys
-
-#
-# Complete the 'compareTriplets' function below.
-#
-# The function is expected to return an INTEGER_ARRAY.
-# The function accepts following parameters:
-#  1. INTEGER_ARRAY a
-#  2. INTEGER_ARRAY b
-#
 
 def compareTriplets(a, b):
     al,bo=0,0
